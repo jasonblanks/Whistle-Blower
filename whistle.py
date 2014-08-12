ccbad8cfa6b886bd82785804a8d4f429643ac2f7
jasonblanks <jason.blanks@gmail.com>
2014-08-12 11:33:37 -0400

    Update whistle.py

--- whistle.py
+++ whistle.py
@@ -4,6 +4,8 @@ import os
 from rekall import session
 from rekall import plugins
 from rekall.plugins.addrspaces import standard
+from rekall import interactive
+
 from sys import platform as _platform
 
 if _platform == "win32":
@@ -53,22 +55,43 @@ def RegScan():
 
 def memscan():
     #logging.getLogger().setLevel(logging.DEBUG)
-
+    livecapture = True
     OS = OSCheck()
+    share=r"\\VBOXSVR\Temp"
     if OS == r"win32" or OS == r"win64":
-        system("winpmem_1.6.0.exe -u")
-        system("winpmem_1.6.0.exe -l")
+        if livecapture == True:
+            root = os.getcwd()
+            os.system("winpmem_1.6.0.exe -u")
+            os.system("winpmem_1.6.0.exe -l")
+            os.system("winpmem_1.6.0.exe "+share+"\\image.raw")
+            s = session.Session(
+                filename=share+"\\image.raw",
+                #interactive.ImportEnvironment(filename=r"\\.\pmem"),
+                profile_path=["http://profiles.rekall-forensic.com"])
 
-        s = session.Session(
-            filename="/home/jason/PycharmProjects/Whistle-Blower/Win7SP1x86.raw",
-            profile_path=["http://profiles.rekall-forensic.com"])
+        else:
+            os.system("winpmem_1.6.0.exe -u")
+            os.system("winpmem_1.6.0.exe -l")
+
+            #s = interactive.ImportEnvironment(filename=r"\\.\pmem")
+            s = session.Session(
+                #filename=r"\\.\pmem",
+                #interactive.ImportEnvironment(filename=r"\\.\pmem"),
+                profile_path=["http://profiles.rekall-forensic.com"])
 
         print s.GetParameter("profile")
         print  s.plugins.pslist()
-        system("winpmem_1.6.0.exe -u")
+        os.system("winpmem_1.6.0.exe -u")
 
     else:
-        print "Only Windows Memory Analysis is supported at this time"
+        #print "Only Windows Memory Analysis is supported at this time"
+        s = session.Session(
+            filename="/home/me/PycharmProjects/Whistle-Blower/Win7SP1x86.raw",
+            profile_path=["http://profiles.rekall-forensic.com"])
+
+        print s.GetParameter("profile")
+        print  s.plugins.malfind()
+        #system("winpmem_1.6.0.exe -u")
         #with s:
         #    s.physical_address_space = standard.FDAddressSpace(fhandle=open(
         #            "/home/jason/PycharmProjects/Whistle-Blower/Win7SP1x86.raw"), session=s)
@@ -87,6 +110,6 @@ def main():
         memscan()
     else:
         print "System is a "+OS+" Machine, nothing to be done here."
-
+        memscan()
 
 main()
